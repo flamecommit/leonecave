8b93cde58899096357b6c362e51890212702d9a6
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-08-25 18:12:37 +0900

    pinterest grid

--- pages/artworks/index.vue
+++ pages/artworks/index.vue
@@ -16,6 +16,56 @@
         <span class="material-symbols-outlined">navigate_next</span>
       </button>
     </div>
+    <!-- <div class="masonry-container">
+      <div class="masonry-item s">
+        <span>1</span>
+      </div>
+      <div class="masonry-item m">
+        <img src="~images/the/qrcode.webp" alt="">
+      </div>
+      <div class="masonry-item l">
+        <span>3</span>
+      </div>
+      <div class="masonry-item l">
+        <span>4</span>
+      </div>
+      <div class="masonry-item s">
+        <span>5</span>
+      </div>
+      <div class="masonry-item l">
+        <span>6</span>
+      </div>
+      <div class="masonry-item l">
+        <span>7</span>
+      </div>
+      <div class="masonry-item l">
+        <span>8</span>
+      </div>
+      <div class="masonry-item s">
+        <span>9</span>
+      </div>
+      <div class="masonry-item m">
+        <span>10</span>
+      </div>
+      <div class="masonry-item s">
+        <span>11</span>
+      </div>
+      <div class="masonry-item m">
+        <span>12</span>
+      </div>
+      <div class="masonry-item l">
+        <span>13</span>
+      </div>
+      <div class="masonry-item s">
+        <span>14</span>
+      </div>
+      <div class="masonry-item m">
+        <span>15</span>
+      </div>
+      <div class="masonry-item s">
+        <span>16</span>
+      </div>
+    </div> -->
   </main>
 </template>
 
@@ -64,6 +114,36 @@ function onSwiper(swiper) {
       .material-symbols-outlined {
         font-size: 36px;
       }
+    }
+  }
+  .masonry-container {
+    max-width: 100%;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 260px);
+    grid-auto-rows: 10px;
+    justify-content: center;
+  }
+  .masonry-item {
+    background: #333;
+    margin: 5px;
+    font-size: 30px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    grid-row-end: span auto;
+
+    &.s {
+      grid-row-end: span 26;
+    }
+    &.m {
+      grid-row-end: span 33;
+    }
+    &.l {
+      grid-row-end: span 45;
+    }
+    img {
+      width: 100%;
     }
   }
 }
